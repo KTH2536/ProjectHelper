13c35f468686bdad36f0fcb2a7f4675e8c3300bf
Bihan <direction5426@gmail.com>
2021-06-02 11:24:03 +0900

    Initialize

---
+++ src/main/java/helper/ProjectHelper/ProjectHelperApplication.java
@@ -0,0 +1,13 @@
+package helper.ProjectHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/helper/ProjectHelper/ProjectHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package helper.ProjectHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
